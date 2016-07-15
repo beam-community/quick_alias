174ec0bb5797c6215bc03895a074cbd4137b67b9
Blake Williams <blake@blakewilliams.me>
2016-07-15 11:38:22 -0400

    Setup hex package meta

--- mix.exs
+++ mix.exs
@@ -7,26 +7,26 @@ defmodule QuickAlias.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     package: package]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      maintainers: ["Blake Williams", "George Brocklehurst"],
+      links: %{
+        "GitHub" => "https://github.com/thoughtbot/quick_alias"
+      },
+      description: "Quick aliases for .iex.exs",
+    ]
   end
 end
